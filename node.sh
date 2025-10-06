e207c4a8d040a5265c2c64c6a8348742f8db0777
Celso Benedetti <celsobenedetti2@gmail.com>
2025-10-05 21:56:09 -0300

    tweak: use mise for node

--- node.sh
+++ node.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# 1 install fnm
-# 2 install node version
-# 3 install pnpm
+# The below is
+# # 1 install fnm
+# # 2 install node version
+# # 3 install pnpm
+#
+# # 1
+# cargo install fnm
+#
+# export PATH=$PATH:$HOME/.cargo/bin
+#
+# # 2
+# fnm install --lts
+#
+# # 3. install pnpm
+# curl -fsSL https://get.pnpm.io/install.sh | sh -
 
-# 1
-cargo install fnm
+# ommarcy
 
-export PATH=$PATH:$HOME/.cargo/bin
-
-# 2
-fnm install --lts
-
-# 3. install pnpm
-curl -fsSL https://get.pnpm.io/install.sh | sh -
+mise settings add idiomatic_version_file_enable_tools node

eb241d02c726819c99f158770482dcaf52636f1b
Celso Benedetti <celsobenedetti2@gmail.com>
2026-01-09 16:19:58 -0300

    add(pacman): gum

--- pacman.sh
+++ pacman.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 yay -S \
-    wget \
-    stow \
-    tmux \
-    mkcert \
-    age \
-    zk \
-    timer \
-    openpomodoro \
-    just \
-    tmuxinator \
-    hyprsunset
+  wget \
+  stow \
+  tmux \
+  mkcert \
+  age \
+  zk \
+  timer \
+  openpomodoro \
+  just \
+  tmuxinator \
+  hyprsunset \
+  gum
 
 # aur
 yay -S \
-    ngrok
+  ngrok
 
 # make mise respect nvmrc
 mise settings add idiomatic_version_file_enable_tools node
